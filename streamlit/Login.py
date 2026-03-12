220f39cb57fce01726ae8ff44c323d2a5515590c
Shivani Khot <shivani.khot@gmail.com>
2026-03-12 09:30:41 +0530

    fix switch_page path

--- streamlit/Login.py
+++ streamlit/Login.py
@@ -65,7 +65,7 @@ if st.button("Login"):
     if email and password == "scalar@123":
         st.session_state.logged_in = True
         st.success("Login Successful ✅")
-        st.switch_page("streamlit/pages/1_CEO.py")
+        st.switch_page("pages/1_CEO.py")
     else:
         st.error("Invalid Email or Password ❌")
         
